f261381cabc40e218c3061c5028da03c06c7076a
yinzhen <yinzhenjob@outlook.com>
2017-08-28 14:48:19 +0800

    创建Home模块文件，以及Network模块文件

---
+++ ShanBoMuseum/Home/HomeModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeModel.swift
+//  ShanBoMuseum
+//
+//  Created by 曾政桦 on 2017/8/28.
+//  Copyright © 2017年 隐贞. All rights reserved.
+//
+
+import UIKit
+
+class HomeModel {
+
+}
---
+++ ShanBoMuseum/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ShanBoMuseum
+//
+//  Created by 曾政桦 on 2017/8/28.
+//  Copyright © 2017年 隐贞. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
